f88f0c3e62f1d5c391d767c992db138eaa71bf12
dubeyxfr <prashant250971@gmail.com>
2025-07-23 21:26:00 +0530

    Typeconnverston.java

--- Typeconnverston.java
+++ Typeconnverston.java
@@ -2,8 +2,8 @@ public class Typeconnverston {
     public static void main(String[] args) {
         float myFloat = 5;
         System.out.println(myFloat);
-        //here 5 will change into the 5.0 in Float value becasuse of the explicit
-        //implicite
+        //here 5 will change into the 5.0 in Float value becasuse of the implicite
+        //explicite
         int myValue = (int) 5f;
         System.out.println(myValue);
     }
