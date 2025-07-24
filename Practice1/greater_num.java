f4c35614ffa9bf0af9292ab57df4a6c6b905d922
Prashant Dubey <prashant250971@gmail.com>
2025-07-24 23:51:31 +0530

    Add files via upload

---
+++ Practice1/greater_num.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class greater_num {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter the first digits ");
+        int n1 = input.nextInt();
+        System.out.print("enter the second digits ");
+        int n2 = input.nextInt();
+        System.out.print("enter the third digits ");
+        int n3 = input.nextInt();
+        if(n1>n2 && n1>n3){
+            System.out.println("n1 is grater num");
+        } else if (n2>n1 && n2>n3) {
+            System.out.println("n2 is greater num ");
+
+        }else{
+            System.out.println("n3 is greater num");
+        }
+    }
+}
