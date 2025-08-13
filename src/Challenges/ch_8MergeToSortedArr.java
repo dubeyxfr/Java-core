f2692d1b5eae75a61d0da6ad7f08bbae2d540326
dubeyxfr <prashant250971@gmail.com>
2025-08-13 23:18:29 +0530

    update

--- src/Challenges/ch_8MergeToSortedArr.java
+++ src/Challenges/ch_8MergeToSortedArr.java
@@ -3,8 +3,8 @@ package Challenges;
 public class ch_8MergeToSortedArr {
     public static void main(String[] args) {
         System.out.println("welcome to the Merge to sorted Array");
-        int [] arr1 = ArrayUtility.intputArray();
-        int [] arr2 = ArrayUtility.intputArray();
+        int [] arr1 = ArrayUtility.intputArray();//input first Arr
+        int [] arr2 = ArrayUtility.intputArray();//input second Arr
         int [] mergeArr = Merge(arr1, arr2);
         System.out.println("your merge Array is: ");
         ArrayUtility.displayarr(mergeArr);
@@ -12,18 +12,18 @@ public class ch_8MergeToSortedArr {
 
     }
     public static int [] Merge(int[] arr1, int [] arr2){
-        int newSize = arr1.length + arr2.length;
-        int[] newArr = new int [newSize];
-        int i = 0 , j = 0, k = 0;
-        while (i< arr1.length || j < arr2.length){
-            if (j == arr2.length || i < arr1.length && arr1[i] < arr2[j]){
-                newArr[k] = arr1[i];
-                i++;
-                k++;
+        int newSize = arr1.length + arr2.length;//add the size of two Arr
+        int[] newArr = new int [newSize]; // make the new Arr values by the Copy from new size Arr
+        int i = 0 , j = 0, k = 0; //insilazations of the 3 arr
+        while (i< arr1.length || j < arr2.length){ //if arr length small from i or j means his own insalizations start the loop
+            if (j == arr2.length || i < arr1.length && arr1[i] < arr2[j]){ //copy conditions
+                newArr[k] = arr1[i]; //copy arr values
+                i++; //increment
+                k++; //increment
             }else{
                 newArr[k] = arr2[j];
-                j++;
-                k++;
+                j++; //increment
+                k++; //increment
             }
         }
 
