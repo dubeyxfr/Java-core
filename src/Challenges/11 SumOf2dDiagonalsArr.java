063dcc0bc985a6431aa75664c9209f988eb7e2fb
dubeyxfr <prashant250971@gmail.com>
2025-08-15 23:52:03 +0530

    Update

--- src/Challenges/11 SumOf2dDiagonalsArr.java
+++ src/Challenges/11 SumOf2dDiagonalsArr.java
@@ -3,6 +3,7 @@ package Challenges;
 class SumOf2dDaigonalsArr {
     public static void main(String[] args) {
         System.out.println("welcome to the diagonal Sum");
+        System.out.println("Only for the Rows == columns");
         int[][] myArr = ArrayUtility.intput2DArray();
         long sum = sumDiagonals(myArr);
         System.out.println("your Diagonals Sum is = " +sum);
@@ -12,22 +13,17 @@ class SumOf2dDaigonalsArr {
         long leftSum = sumLeftDiagonals(myArr);
         long rightSum = sumRightDiagonals(myArr);
         long sum = leftSum + rightSum;
-        if (myArr.length % 2 != 0){
+        if (myArr.length % 2 != 0){ //Remove center element if matrix size is odd
             int ind = myArr.length / 2;
             sum -=myArr[ind][ind];
-
-
         }
-
-
-
         return sum;
     }
     public static long sumLeftDiagonals(int[][] myArr){
         int sum = 0;
         int i = 0;
         while(i < myArr.length){
-            sum +=myArr[i][i];
+            sum +=myArr[i][i]; //logic tells the adds the main elements
             i++;
         }
         return sum;
@@ -36,7 +32,7 @@ class SumOf2dDaigonalsArr {
         int sum = 0;
         int i = 0;
         while (i < myArr.length){
-            int col = myArr.length - 1 - i;
+            int col = myArr.length - 1 - i; //Reverse column index
             sum += myArr[i][col];
             i++;
         }
