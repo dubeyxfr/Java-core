c3d6383bd473261fe6a1a1a0e63aeff144acac8f
dubeyxfr <prashant250971@gmail.com>
2025-08-20 21:58:17 +0530

    new

--- src/ObjectJava/Car.java
+++ src/ObjectJava/Car.java
@@ -11,20 +11,22 @@ public class Car {
     int noOfSeats;
 
     //Methods of the car
-    public void Driving(){
+    public void start (){
         if (CurrentFuleInLitter <= 0){
-            System.out.println("Your car is out of fuel");
+            System.out.println("Your car is out of fuel, Can not Start");
         } else if (CurrentFuleInLitter < 5) {
             System.out.println("Your car is in the Reserved mode, Please Refuel it ");
-            
         }else {
-            System.out.println("Car is Driving");
-            CurrentFuleInLitter--;
+            System.out.println("Car is Started, bruhhhhhh.....");
+
         }
+    }
+    public void Driving(){
+        CurrentFuleInLitter--;
+        System.out.println("Car is Driving...");
     }
     public void addFuel(float fuel){
         CurrentFuleInLitter += fuel;
-
     }
     public float GetCurretFuelLevel(){
         return CurrentFuleInLitter;
--- src/ObjectJava/Drive.java
+++ src/ObjectJava/Drive.java
@@ -3,15 +3,19 @@ package ObjectJava;
 public class Drive {
     public static void main(String[] args) {
         //
-        Car myCar = new Car();
-
-        myCar.addFuel(6);
-
-        myCar.Driving();
-        myCar.Driving();
-        myCar.Driving();
-        myCar.addFuel(5);
-        System.out.println(myCar.GetCurretFuelLevel());
+//        Car myCar = new Car();
+//
+//        myCar.addFuel(6);
+//
+//        myCar.Driving();
+//        myCar.Driving();
+//        myCar.Driving();
+//        myCar.addFuel(5);
+//        System.out.println(myCar.GetCurretFuelLevel());
+        Car Swift = new Car();
+        Swift.addFuel(9);
+        Swift.start();
+        Swift.Driving();
 
     }
 }
