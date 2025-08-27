bf11658ca4f541d6bb4fb1f79fc8483e193e489c
dubeyxfr <prashant250971@gmail.com>
2025-08-27 22:44:27 +0530

    UpDate

--- src/Control_Statments/Ternary_Opreator.java
+++ src/Control_Statments/Ternary_Opreator.java
@@ -10,14 +10,18 @@ public class Ternary_Opreator {
         int num1 = input.nextInt();
         System.out.print("Enter the Second Number = ");
         int num2 = input.nextInt();
-        if (num1 < num2){
-            System.out.println(num2 + " is the Grater Number");
+//        if (num1 < num2){
+//            System.out.println(num2 + " is the Grater Number");
+//
+//        } else if (num1 == num2) {
+//            System.out.println(num1 +" and " + num2 + " are Equals so please provide Differents numbers");
+//
+//        } else {
+//            System.out.println(num1  + " is the Grater Number");
+//        }
+        int graternumber = num1 > num2 ? num1 : num2;
+        System.out.println(graternumber + " is the Grater number");
 
-        } else if (num1 == num2) {
-            System.out.println(num1 +" and " + num2 + " are Equals so please provide Differents numbers");
 
-        } else {
-            System.out.println(num1  + " is the Grater Number");
-        }
     }
 }
