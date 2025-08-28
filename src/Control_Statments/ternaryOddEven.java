c3c4730b5cd05d19fb0b2da9c065c99fbf628bb2
dubeyxfr <prashant250971@gmail.com>
2025-08-28 22:11:23 +0530

    new

---
+++ src/Control_Statments/ternaryOddEven.java
@@ -0,0 +1,18 @@
+package Control_Statments;
+
+import java.util.Scanner;
+
+public class ternaryOddEven {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the odd even FInder\n");
+        System.out.print("Enter the Number you Want to Find: ");
+        int num = input.nextInt();
+        ternaryOddEven(num);
+
+    }
+    public static void ternaryOddEven(int num){
+        String numEvenOdd = num  % 2 == 0 ? "even" : "Odd";
+        System.out.println("Your Number is " + numEvenOdd);
+    }
+}
