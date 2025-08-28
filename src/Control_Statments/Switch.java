7622e3b32f4af19accc444308a2453fa5d33cd6a
dubeyxfr <prashant250971@gmail.com>
2025-08-28 21:11:25 +0530

    new

---
+++ src/Control_Statments/Switch.java
@@ -0,0 +1,38 @@
+package Control_Statments;
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Day of the Week Detector\n");
+        System.out.print("Enter your day in num ");
+        int day = input.nextInt();
+        switch (day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid Day");
+                break;
+        }
+    }
+}
