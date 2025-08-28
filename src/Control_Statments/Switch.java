2dc5f42d6b01a43ed0e8cd000100c373a16ea4ed
dubeyxfr <prashant250971@gmail.com>
2025-08-28 21:31:16 +0530

    new

--- src/Control_Statments/Switch.java
+++ src/Control_Statments/Switch.java
@@ -8,31 +8,56 @@ public class Switch {
         System.out.println("Welcome to Day of the Week Detector\n");
         System.out.print("Enter your day in num ");
         int day = input.nextInt();
-        switch (day){
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("Invalid Day");
-                break;
+        newSwitch(day);
+        oldStr(day);
+
+
+        }
+        public static void newSwitch(int day){
+        String dayStr = switch (day) {
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "wednesday";
+            case 4 -> "thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            case 7 -> "Holiday";
+            default -> "invalid";
+
+
+        };
+            System.out.println(dayStr);
+
+
+
+        }
+        public static void oldStr(int day){
+            switch (day){
+                case 1:
+                    System.out.println("Monday");
+                    break;
+                case 2:
+                    System.out.println("Tuesday");
+                    break;
+                case 3:
+                    System.out.println("Wednesday");
+                    break;
+                case 4:
+                    System.out.println("Thursday");
+                    break;
+                case 5:
+                    System.out.println("Friday");
+                    break;
+                case 6:
+                    System.out.println("Saturday");
+                    break;
+                case 7:
+                    System.out.println("Sunday");
+                    break;
+                default:
+                    System.out.println("Invalid Day");
+                    break;
+
         }
     }
 }
---
+++ src/Control_Statments/switch2_0.java
@@ -0,0 +1,7 @@
+package Control_Statments;
+
+public class switch2_0 {
+    public static void main(String[] args) {
+
+    }
+}
