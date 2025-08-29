df8e6437c92c46fcb20fecd554cdb405341e8137
dubeyxfr <prashant250971@gmail.com>
2025-08-29 22:56:58 +0530

    new

---
+++ src/Control_Statments/Short.java
@@ -0,0 +1,15 @@
+package Control_Statments;
+
+import java.util.Scanner;
+
+public class Short{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println((a < b ? a : b) + " is the minimum number");
+    }
+}
