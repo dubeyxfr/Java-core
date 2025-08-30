51f2739211f610f3d6bbf7039ad99b946e88cf85
dubeyxfr <prashant250971@gmail.com>
2025-08-30 22:33:23 +0530

    new

---
+++ src/Control_Statments/MonthSwitch.java
@@ -0,0 +1,10 @@
+package Control_Statments;
+
+import java.util.Scanner;
+
+public class MonthSwitch {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the months mapping");
+    }
+}
