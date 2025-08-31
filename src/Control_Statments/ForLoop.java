85ae0a71fe7802db52f230113b46703c8f1d8966
dubeyxfr <prashant250971@gmail.com>
2025-08-31 14:33:11 +0530

    code

--- src/Control_Statments/ForLoop.java
+++ src/Control_Statments/ForLoop.java
@@ -1,4 +1,16 @@
 package Control_Statments;
 
 public class ForLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<=100){
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println("\nBy for Loop");
+
+        for (int j = 0 ; j <= 100 ; j++){
+            System.out.print(j +  " ");
+        }
+    }
 }
