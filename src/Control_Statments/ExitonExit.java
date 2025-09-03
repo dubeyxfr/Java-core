1c11ada312daa3df3f448ab4118243203822f5e7
dubeyxfr <prashant250971@gmail.com>
2025-09-03 22:26:11 +0530

    new

---
+++ src/Control_Statments/ExitonExit.java
@@ -0,0 +1,19 @@
+package Control_Statments;
+
+import java.util.Scanner;
+
+public class ExitonExit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while(true){
+            System.out.print("Enter your Command: ");
+            String command = input.next();
+            if (command.equals("exit")){
+                break;
+
+            }
+
+        }
+        System.out.println("You Successfully exit");
+    }
+}
