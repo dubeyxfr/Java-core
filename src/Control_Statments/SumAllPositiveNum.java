f098fa36cd16e816ab87fcea50861de95b67f851
dubeyxfr <prashant250971@gmail.com>
2025-09-04 23:20:52 +0530

    code

---
+++ src/Control_Statments/SumAllPositiveNum.java
@@ -0,0 +1,24 @@
+package Control_Statments;
+
+import Challenges.ArrayUtility;
+
+import java.util.Scanner;
+
+public class SumAllPositiveNum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the All add positive num\n");
+        int[] numArr = ArrayUtility.intputArray();
+        int sum = 0;
+        for (int num : numArr){
+            if (num < 0){
+                continue;
+            }
+            sum += num;
+
+
+
+        }
+        System.out.println("Your Number sum is " +sum);
+    }
+}
