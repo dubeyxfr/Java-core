52814c7e12da180973234f1bf90566f21e3dd7f2
dubeyxfr <prashant250971@gmail.com>
2025-09-05 21:59:43 +0530

    new

---
+++ src/Control_Statments/PrintEvenNUm.java
@@ -0,0 +1,22 @@
+package Control_Statments;
+
+import Challenges.ArrayUtility;
+
+import java.util.Scanner;
+
+public class PrintEvenNUm {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Even number\n");
+        for (int i = 1 ;i<= 100 ; i++){
+            if (i % 2 == 1){
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+
+
+
+
+
+    }
+}
