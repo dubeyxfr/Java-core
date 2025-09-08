d2444ac763c9978084243339aa4faf8de31ff96a
dubeyxfr <prashant250971@gmail.com>
2025-09-08 09:19:43 +0530

    new

--- src/Control_Statments/PMath.java
+++ src/Control_Statments/PMath.java
@@ -2,6 +2,13 @@ package Control_Statments;
 
 public class PMath {
     public static void main(String[] args) {
-        System.out.println(Math.abs(-99));
+//        System.out.println(Math.abs(-99));
+//        System.out.println(Math.ceil(12.09));
+//        System.out.println(Math.floor(12.07));
+//        System.out.println(Math.round(12.04));
+//        System.out.println(Math.random());
+        long random = Math.round(Math.random());
+        System.out.println("your num is " + random);
+
     }
 }
