6de3a6ef0a1cea8909f49a470fe6c83a1b2f627f
dubeyxfr <prashant250971@gmail.com>
2025-09-08 18:16:53 +0530

    new

--- src/Control_Statments/StringFormate.java
+++ src/Control_Statments/StringFormate.java
@@ -3,12 +3,12 @@ package Control_Statments;
 public class StringFormate {
     public static void main(String[] args) {
         String name = "sandeep";
-        int marks = 45;
+        int marks = 45654;
 
         // printf method
-        System.out.printf("Hello %s, got the %d marks in the maths subject%n", name, marks);
+        System.out.printf("Hello %s, got the %,5d marks in the maths subject%n", name, marks);
 
         // OR format method
-        System.out.format("Hello %s, got the %d marks in the maths subject%n", name, marks);
+        System.out.format("Hello %10s, got the %d marks in the maths subject%n", name, marks);
     }
 }
