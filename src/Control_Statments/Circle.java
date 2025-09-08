21f772310a4487ed624f76e2ba5069a58e0641ed
dubeyxfr <prashant250971@gmail.com>
2025-09-08 19:48:14 +0530

    new

---
+++ src/Control_Statments/Circle.java
@@ -0,0 +1,44 @@
+package Control_Statments;
+
+import java.util.Scanner;
+
+class Circle {
+    double radius;
+
+    // Constructor
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // Circumference of Circle
+    double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    // Area of Circle
+    double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    // toString override
+
+
+    @Override
+    public String toString() {
+        return "Circle " +"Radius = " + radius +"\ngetCircumference = " + getCircumference() + "\ngetArea = " +getArea();
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the Circle Program\n");
+        System.out.print("Please Enter the Radius of the Circle: ");
+        double radius = input.nextDouble(); // double use kar better accuracy ke liye
+
+        Circle circle = new Circle(radius);
+
+        // Print circle details
+        System.out.println(circle.toString());
+    }
+}
+
---
+++ src/Control_Statments/ConcatenateOrConverter.java
@@ -0,0 +1,16 @@
+package Control_Statments;
+
+public class ConcatenateOrConverter {
+    public static void main(String[] args) {
+        String num = "123";
+        String name ="Prashant";
+        String result = num.concat(" ").concat(name);
+        System.out.println(result);
+        System.out.println(name.toUpperCase());
+
+        //converter to str to int.
+        String str = "1234";
+
+
+    }
+}
