aa7484520a3d74ddd07613697f6ec139ededcc09
dubeyxfr <prashant250971@gmail.com>
2025-09-09 18:27:08 +0530

    code

---
+++ src/Challenges/ArrayToString.java
@@ -0,0 +1,14 @@
+package Challenges;
+
+public class ArrayToString {
+    public static void main(String[] args) {
+        String [] arr = new String[] {
+                "this ", "is " , "the " , "String " , "for " , "Test "
+        };
+        StringBuilder sb = new StringBuilder();
+        for (String str : arr) {
+            sb.append(str).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
