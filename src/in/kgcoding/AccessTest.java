cdc1e39dbc512c7488104a6d12999761e3401f9f
dubeyxfr <prashant250971@gmail.com>
2025-09-14 10:58:14 +0530

    new

--- src/in/kgcoding/AccessTest.java
+++ src/in/kgcoding/AccessTest.java
@@ -2,10 +2,13 @@ package in.kgcoding;
 
 public class AccessTest {
     public static void main(String[] args) {
-        Car car = new Car();
-        car.color = "red";
-        car.model = "BMW12";
-        car.FuelLevel//not Allow to Access bcs in obj we defined as a private.
+        Car car = new Car("Black","BMW",20,12000);
+        //car.FuelLevel//not Allow to Access bcs in obj we defined as a private.
+
+        Car newCar = new Car("blue","lambo",20,1200000);
+        System.out.println(newCar);
+
+
 
 
 
--- src/in/kgcoding/Car.java
+++ src/in/kgcoding/Car.java
@@ -3,8 +3,26 @@ package in.kgcoding;
 public class Car {
     public String color;
     public String model;
-    private double FuelLevel;
-    private long CostOfPurchase;
+    double FuelLevel;
+    long CostOfPurchase;
 
+    public Car() {
+    }
 
+    public Car(String color, String model, double fuelLevel, long costOfPurchase) {
+        this.color = color;
+        this.model = model;
+        FuelLevel = fuelLevel;
+        CostOfPurchase = costOfPurchase;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", model='" + model + '\'' +
+                ", FuelLevel=" + FuelLevel +
+                ", CostOfPurchase=" + CostOfPurchase +
+                '}';
+    }
 }
