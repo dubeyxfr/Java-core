b00da45a9c5e954e5a461828771c849825cb7696
dubeyxfr <prashant250971@gmail.com>
2025-09-15 20:42:14 +0530

    code

--- src/in/kgcoding/utils/Calculator.java
+++ src/in/kgcoding/utils/Calculator.java
@@ -7,6 +7,8 @@ public class Calculator {
     public static void main(String[] args) {
         Circle cir = new Circle(5.5);
         Rectangle rect = new Rectangle(3.3,4.5);
-        System.out.printf("Area of the Circle: %s, Area of the RectAngle: %s",cir,rect);
+        double cirarea = Math.PI * Math.pow(cir.radius, 2);
+        double Arearect = rect.breath*rect.length;
+        System.out.println("Area of the circle: " +cirarea + "\nArea of the rectangle: " + Arearect);
     }
 }
