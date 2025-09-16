735820566637397a7f02ac70e36f5b499b25a419
dubeyxfr <prashant250971@gmail.com>
2025-09-16 10:19:06 +0530

    code

--- src/in/kgcoding/ch77/TestEmployee.java
+++ src/in/kgcoding/ch77/TestEmployee.java
@@ -3,6 +3,7 @@ package in.kgcoding.ch77;
 public class TestEmployee {
     public static void main(String[] args) {
         Employee emp = new Employee("Prashant",21,120000);
+        System.out.println(emp.getEmployeeDetails());
 
     }
 }
