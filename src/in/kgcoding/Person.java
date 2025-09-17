cad97fb8d8829dae4a44281f22593edfa50b3e0f
dubeyxfr <prashant250971@gmail.com>
2025-09-17 12:07:04 +0530

    code

---
+++ src/in/kgcoding/Person.java
@@ -0,0 +1,46 @@
+package in.kgcoding;
+
+public class Person {
+    private String name;
+    private String age;
+    private String id;
+
+    public Person(String name, String age, String id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
