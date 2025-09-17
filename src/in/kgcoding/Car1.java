02dc2ad2d35397edb72f3ff0f0b3091099873ea7
dubeyxfr <prashant250971@gmail.com>
2025-09-17 17:25:14 +0530

    code

---
+++ src/in/kgcoding/Car1.java
@@ -0,0 +1,22 @@
+package in.kgcoding;
+
+public class Car1 {
+    private int noOfDoors;
+
+    // repair method uses Tire class
+    public void repair () {
+        Tire t = new Tire();
+
+    }
+
+    // Inner class Tire
+    protected  class Tire {
+        private double width;
+        private double pressure;
+        private String material;
+    }
+    public void inflate() {
+        noOfDoors = 4;
+
+    }
+}
---
+++ src/in/kgcoding/Challange80/CHallange80.java
@@ -0,0 +1,14 @@
+package in.kgcoding.Challange80;
+
+public class CHallange80 {
+    private String itemId;
+    private String title;
+    private String Author;
+
+    public void CheckOut(){
+        System.out.println("Your Book is Checkout Sucessfully");
+    }
+    public void Return () {
+        System.out.println("Returned!!");
+    }
+}
