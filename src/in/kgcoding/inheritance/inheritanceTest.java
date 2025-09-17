441757dd28f88883d724e254d0720c5c45521dcf
dubeyxfr <prashant250971@gmail.com>
2025-09-17 11:59:10 +0530

    code

--- src/in/kgcoding/inheritance/inheritanceTest.java
+++ src/in/kgcoding/inheritance/inheritanceTest.java
@@ -4,6 +4,9 @@ public class inheritanceTest {
     public static void main(String[] args) {
         Vehicle vehicle = new Vehicle();
         vehicle.commute();
+        System.out.println(vehicle.toString());
+        System.out.println(vehicle.hashCode());
+        System.out.println(vehicle.getClass());
         TwoWheelers two = new TwoWheelers();
         two.commute();
         two.balance();
