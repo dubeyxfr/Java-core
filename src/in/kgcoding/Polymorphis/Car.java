9ab88314a68cf97a71b08a646c2970ee4b4b1142
dubeyxfr <prashant250971@gmail.com>
2025-09-21 12:01:34 +0530

    new

--- src/in/kgcoding/Polymorphis/Car.java
+++ src/in/kgcoding/Polymorphis/Car.java
@@ -1,8 +1,10 @@
 package in.kgcoding.Polymorphis;
 
-public class Car extends vechile{
-    public void noOfDoors
-        int noOfdoors =4;
+public class Car extends Vehicle {
+    int noOfDoors = 4;   // field/attribute
 
+    public void displayDoors() {   // method to use it
+        System.out.println("Number of doors: " + noOfDoors);
     }
 }
+
