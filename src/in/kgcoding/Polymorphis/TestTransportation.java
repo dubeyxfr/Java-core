a79c11874a283a946be024587c1aff4b778f0b48
dubeyxfr <prashant250971@gmail.com>
2025-09-21 14:13:30 +0530

    code

--- src/in/kgcoding/Polymorphis/TestTransportation.java
+++ src/in/kgcoding/Polymorphis/TestTransportation.java
@@ -7,6 +7,8 @@ public class TestTransportation {
 
         Vehicle Vcar = new Car(); // here dont need the type casting
 
-        Car vCar = (Car) new Vehicle(); // here we need ot type casting for the error
+        Car vCar = (Car) new Vehicle();
+        // here we need ot type casting for the error
+        Object ob = new Car(); //we can do this with the default object.
     }
 }
