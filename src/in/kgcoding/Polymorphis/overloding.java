1c01b34d45c8bf31b052c00a15a2bb238b73d881
dubeyxfr <prashant250971@gmail.com>
2025-09-22 10:25:50 +0530

    new

--- src/in/kgcoding/Polymorphis/overloding.java
+++ src/in/kgcoding/Polymorphis/overloding.java
@@ -4,9 +4,14 @@ public class overloding {
    public static int sum(int a, int b){
        return a + b;
    }
+   public static int sum(int a,int b,int c,int d){
+       return a + b+ c+ d;
+   }
 
     public static void main(String[] args) {
-        overloding sum = new overloding();
-        System.out.println(overloding.sum(7,3));
+        overloding overload = new overloding();
+        int sum = overloding.sum(4,5);
+        System.out.println(overloding.sum(7,sum));
+        System.out.println(overloding.sum(8,2));
     }
 }
