053398f4580c6d7f0e249f2903a502360f82d824
dubeyxfr <prashant250971@gmail.com>
2025-09-26 13:19:52 +0530

    new

--- src/in/kgcoding/Practice/Car2.java
+++ src/in/kgcoding/Practice/Car2.java
@@ -5,6 +5,11 @@ public class Car2 extends Vehicle_1 {
 
     public Car2(String services, String carModels, int AmountForServices) {
         super(services, carModels, AmountForServices);
+        if (AmountForServices >= 400){
+            System.out.println("your Washing is Started");
+        }else{
+            System.out.println("your Washing not Started washing Price = 400rs ");
+        }
     }
 }
 
--- src/in/kgcoding/Practice/TestForCar.java
+++ src/in/kgcoding/Practice/TestForCar.java
@@ -2,8 +2,9 @@ package in.kgcoding.Practice;
 
 public class TestForCar {
     public static void main(String[] args) {
-        Car2 car = new Car2("Car Wasing","BMW",500);
+        Car2 car = new Car2("Car Washing","BMW",500);
         car.setAmountForServices(400);
+        System.out.println(car);
 
 
 
