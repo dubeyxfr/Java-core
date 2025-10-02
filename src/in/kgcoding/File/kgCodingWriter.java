b5aea04b1304ae4d95a9d6b16a102b45c99a5e74
dubeyxfr <prashant250971@gmail.com>
2025-10-02 18:12:30 +0530

    new

--- src/in/kgcoding/File/kgCodingWriter.java
+++ src/in/kgcoding/File/kgCodingWriter.java
@@ -8,9 +8,6 @@ public class kgCodingWriter {
         String fileName = "Java-course.txt";
         try (FileWriter writer = new FileWriter(fileName)) {
             writer.write("this is best java course." );
-            for (int i=0;i<10000;i++){
-                writer.write('x');
-            }
             writer.flush();
             System.out.println("File written successful.");
         }catch (IOException exception) {
