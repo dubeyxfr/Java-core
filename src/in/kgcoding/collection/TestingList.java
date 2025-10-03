f4186ae8a04efc9bc01179b49a574506f2d4b0c6
dubeyxfr <prashant250971@gmail.com>
2025-10-03 13:35:07 +0530

    new

--- src/in/kgcoding/collection/TestingList.java
+++ src/in/kgcoding/collection/TestingList.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class TestingList {
     public static void main(String[] args) {
-      List strList = new ArrayList();
+      List<String> strList = new ArrayList<>();
       strList.add("Prashant");
       strList.add(1," Dubey");
       strList.set(1," Singh");
@@ -14,6 +14,9 @@ public class TestingList {
         }
         for (int i = 0;i < strList.size();i++){
             System.out.print(strList.get(i));
+        }
+        for (String str : strList){
+            System.out.print(str);
         }
 
     }
