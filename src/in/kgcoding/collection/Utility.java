435c1e7916c629a2366798b1ea54597f6df711b0
dubeyxfr <prashant250971@gmail.com>
2025-10-03 17:40:15 +0530

    new

---
+++ src/in/kgcoding/collection/Utility.java
@@ -0,0 +1,11 @@
+package in.kgcoding.collection;
+
+import java.util.Collection;
+
+public class Utility {
+    public static <E> void print(Collection<E> collection){
+        for (E coll : collection){
+            System.out.printf("%s ", coll.toString());
+        }
+    }
+}
