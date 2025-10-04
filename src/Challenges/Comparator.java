f5fd65c8fafeb936e49a29766156129822ece65a
dubeyxfr <prashant250971@gmail.com>
2025-10-04 13:33:29 +0530

    code

--- src/Challenges/Comparator.java
+++ src/Challenges/Comparator.java
@@ -14,6 +14,17 @@ public class Comparator {
 
     }
     public static void sortInDescending(List<String> stringList){
-        Collections.sort(stringList);
+        Collections.sort(stringList, new java.util.Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.equals(o2)){
+                    return 0;
+                } else if (o1.charAt(0) < o2.charAt(0)) {
+                    return -1;
+                }else {
+                    return 1;
+                }
+            }
+        });
     }
 }
