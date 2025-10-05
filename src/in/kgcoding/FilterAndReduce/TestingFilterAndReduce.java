c2da309379a4c91a657b132559777ddcdda0424c
dubeyxfr <prashant250971@gmail.com>
2025-10-05 15:20:42 +0530

    new

--- src/in/kgcoding/FilterAndReduce/TestingFilterAndReduce.java
+++ src/in/kgcoding/FilterAndReduce/TestingFilterAndReduce.java
@@ -1,24 +1,22 @@
 package in.kgcoding.FilterAndReduce;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 public class TestingFilterAndReduce {
     public static void main(String[] args) {
-        List<String> fruits = List.of("Apple", "Banana","Mango","Cherry","Date");
+        List<String> fruits = List.of("Apple", "Banana", "Mango", "Cherry", "Date");
+
         System.out.println(fruits.size());
+
         System.out.println("Printing Fruits Normally!!");
         for (String fruit : fruits) {
             System.out.println(fruit);
         }
-//        System.out.println("Printing Fruits by using Stream!!");
-//        fruits.stream().forEach(new Consumer<String>() {
-//            @Override
-//            public void accept(String s) {
-//                System.out.println(s);
-//            }
-//        });
-        System.out.println("By using Lambda...");
-        fruits.stream().forEach(fruit -> System.out.println(fruit));
+
+        // Using Lambda and Filter
+        System.out.println("By using Lambda and Filters");
+        fruits.stream()
+                .filter(fruit -> fruit.endsWith("a"))  // Apply filter
+                .forEach(fruit -> System.out.println(fruit));  // Print filtered results
     }
 }
