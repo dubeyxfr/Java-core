da00d943afc1c5a59b588e16eafd4dfebcd9bb76
dubeyxfr <prashant250971@gmail.com>
2025-10-05 23:08:31 +0530

    new

---
+++ src/Challenges/ch109.java
@@ -0,0 +1,12 @@
+package Challenges;
+
+import java.util.List;
+
+public class ch109 {
+    public static void main(String[] args) {
+        List<Integer> number = List.of(1,2,4,5,6,7,8,9,10);
+        number.stream()
+                .filter(num -> num % 2 == 1)
+                .forEach(num -> System.out.println(num));
+    }
+}
