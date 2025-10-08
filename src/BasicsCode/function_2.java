c0df4525bf1bb3c13e747a0fdc6a008ef9556153
dubeyxfr <prashant250971@gmail.com>
2025-10-08 23:02:33 +0530

    new

--- src/BasicsCode/function_2.java
+++ src/BasicsCode/function_2.java
@@ -1,8 +1,16 @@
 package BasicsCode;
 
-public class function_2 {
-    public static void function_2(){
+import java.util.Scanner;
 
+public class function_2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter Your Number: ");
+        int num = input.nextInt();
+        for (int i = 1; i <= 10; i++){
+            int table = num * i;
+            System.out.printf("%d X %d = %d\n",num,i,table);
+        }
     }
 
 
