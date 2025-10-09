116c6bd595fc843f5f740891dd4f30c5c531cccd
dubeyxfr <prashant250971@gmail.com>
2025-10-09 23:56:17 +0530

    new

--- src/BasicsCode/question10_pattern_orinting.java
+++ src/BasicsCode/question10_pattern_orinting.java
@@ -1,5 +1,7 @@
 package BasicsCode;
 
+import java.util.Scanner;
+
 public class question10_pattern_orinting {
     public static void question10_pattern_orinting(int n) {
 
@@ -25,6 +27,26 @@ public class question10_pattern_orinting {
     }
 
 
+    public static class ArraySum {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter size of array: ");
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+            System.out.println("Enter " + n + " elements:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum += arr[i];
+            }
+
+            System.out.println("Sum of all elements: " + sum);
+        }
+    }
 }
 
 
