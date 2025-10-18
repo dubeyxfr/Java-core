ed017c61e3a436f1122b25546fc44b739226c5f1
dubeyxfr <prashant250971@gmail.com>
2025-10-18 09:45:31 +0530

    new

---
+++ src/Lectures/L6/Largest.java
@@ -0,0 +1,15 @@
+package Lectures.L7_Loops;
+
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        //Finding the Largest of Three numbers
+        
+
+    }
+}
